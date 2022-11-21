72c02dd6b7c056cc5f9f9c4a9c9c173a1f2bdc1a
ImSKH <hero981001@konkuk.ac.kr>
2022-11-22 00:36:55 +0900

    hw

--- 12week/Homework.cpp
+++ 12week/Homework.cpp
@@ -20,7 +20,7 @@ int main(int argc, char** argv){
 	int frame_width = cap.get(CAP_PROP_FRAME_WIDTH);
 	int frame_height = cap.get(CAP_PROP_FRAME_HEIGHT);
 
-	VideoWriter video("sobel_res.avi", VideoWriter::fourcc('M','J','P','G'),10,Size(frame_width,frame_height), False);
+	VideoWriter video("sobel_res.avi", VideoWriter::fourcc('M','J','P','G'),10,Size(frame_width,frame_height), false);
 	printf("open Camera\n");
 
 	Mat img, gray, sobelX, sobelY, sobel;
