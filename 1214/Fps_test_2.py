cc9f78100c9077f74f98671bcf441570cff6a8a2
ImSKH <skihyeon1001@naver.com>
2022-12-14 17:03:11 +0900

    t

---
+++ 1214/Fps_test_2.py
@@ -0,0 +1,28 @@
+import os
+import argparse
+import cv2
+import numpy as np
+import sys
+import time
+from threading import Thread
+import importlib.util
+
+sys.path.append("/home/pi/.local/lib/python3.9/site-packages/")
+
+cap = cv2.VideoCapture(cv2.CAP_V4L2+0)
+cap.set(3, 640)
+cap.set(4, 480)
+fps = cap.get(cv2.CAP_PROP_FPS)
+
+filename = os.__filename__
+
+fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+out = cv2.VideoWriter(filename+'.avi',fourcc,fps,(640,480))
+
+while True:
+	ret, frame = cap.read()
+
+	out.write(frame)
+
+cap.release()
+out.release()
