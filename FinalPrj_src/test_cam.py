f88fb81d5c4d016cb76fd6d81f23cebd7a095b30
ImSKH <skihyeon1001@naver.com>
2022-12-14 11:50:03 +0900

    t

--- FinalPrj_src/test_cam.py
+++ FinalPrj_src/test_cam.py
@@ -6,28 +6,44 @@ import sys
 import time
 from threading import Thread
 import importlib.util
+import copy
 import sys
-
 
 sys.path.append("/home/pi/.local/lib/python3.9/site-packages/tflite_runtime/__init__.py")
 
-MODEL_NAME = '/home/pi/Final/codes/'
-GRAPH_NAME = 'detect.tflite'
-LABELMAP_NAME = 'labelmap.txt'
-min_conf_threshold = 0.5
-resW = 640
-resH = 480
+parser = argparse.ArgumentParser()
+parser.add_argument('--modeldir', default = '/home/pi/Final/codes/')
+parser.add_argument('--graph', default = 'detect.tflite')
+parser.add_argument('--labels', default = 'labelmap.txt')
+parser.add_argument('--threshold', default = 0.5)
+parser.add_argument('--resolution', default = '640x480')
+parser.add_argument('--edgetpu', action = 'store_true')
+
+args = parser.parse_args()
+
+MODEL_NAME = args.modeldir
+GRAPH_NAME = args.graph
+LABELMAP_NAME = args.labels
+min_conf_threshold = float(args.threshold)
+resW, resH = args.resolution.split('x')
 imW, imH = int(resW), int(resH)
-use_TPU = 'store_true'
+use_TPU = args.edgetpu
 
 
 pkg = importlib.util.find_spec('tflite_runtime')
 
 if pkg:
 	from tflite_runtime.interpreter import Interpreter
+	if use_TPU:
+		from tflite_runtime.interpreter import load_delegate
 else:
-	print("failed")
-	sys.exit()
+	from tensorflow.lite.python.interpreter import Interpreter
+	if use_TPU:
+		from tensorflow.lite.python.interpreter import load_delegate
+
+if use_TPU:
+	if (GRAPH_NAME == 'detect.tflite'):
+		GRAPH_NAME = 'edgetpu.tflite'
 
 CWD_PATH = os.getcwd()
 PATH_TO_CKPT = os.path.join(CWD_PATH, MODEL_NAME, GRAPH_NAME)
@@ -36,11 +52,14 @@ PATH_TO_LABELS = os.path.join(CWD_PATH, MODEL_NAME, LABELMAP_NAME)
 with open(PATH_TO_LABELS, 'r') as f:
 	labels = [line.strip() for line in f.readlines()]
 
-
 if labels[0] == '???':
 	del(labels[0])
 
-interpreter = Interpreter(model_path = PATH_TO_CKPT)
+if use_TPU:
+	interpreter = Interpreter(model_path = PATH_TO_CKPT, experimental_delegates = [load_delegate('libedgetpu.so.1.0')])
+	print(PATH_TO_CKPT)
+else:
+	interpreter = Interpreter(model_path = PATH_TO_CKPT)
 interpreter.allocate_tensors()
 
 
@@ -56,12 +75,10 @@ input_std = 127.5
 
 outname = output_details[0]['name']
 
-
 if('StatefulPartitionedCall' in outname):
 	boxes_idx, classes_idx, scores_idx = 1, 3, 0
 else:
 	boxes_idx, classes_idx, scores_idx = 0, 1, 2
-
 
 frame_rate_calc = 1
 freq = cv2.getTickFrequency()
@@ -71,12 +88,10 @@ cap = cv2.VideoCapture(cv2.CAP_V4L2+0)
 cap.set(3, 640)
 cap.set(4, 480)
 
-
 time.sleep(1)
 
 fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-outVideo = cv2.VideoWriter("outVideo_boxed.avi",fourcc,10,(640,480));
-outVideo2 = cv2.VideoWriter("outVideo_blurred.avi",fourcc,10,(640,480));
+outVideo = cv2.VideoWriter("outVideo.avi",fourcc,10,(640,480));
 
 while True:
 	t1 = cv2.getTickCount()
@@ -135,9 +150,8 @@ while True:
 	t2 = cv2.getTickCount()
 	time1 = (t2-t1)/freq
 	frame_rate_calc = 1/time1
-	
-	outVideo.write(boxing_img)
-	outVideo2.write(blurred_img)
+
+	outVideo.write(blurred_img)
 
 	if cv2.waitKey(1) == ord('q'):
 		break
