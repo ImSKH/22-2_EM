b06deb32c0c0c374e72bab784596799368768a4a
ImSKH <skihyeon1001@naver.com>
2022-12-17 17:24:20 +0900

    t

--- Dates/1217/1217_1.py
+++ Dates/1217/1217_1.py
@@ -88,8 +88,8 @@ try:
 			if((scores[i]>min_conf_threshold) and (scores[i]<=1.0)):
 				ymin = int(max(1, (boxes[i][0]*resH)))
 				xmin = int(max(1, (boxes[i][1]*resW)))
-				ymax = int(min(imH, (boxes[i][2]*resH)+5))
-				xmax = int(min(imW, (boxes[i][3]*resW)+5))
+				ymax = int(min(resH, (boxes[i][2]*resH)+5))
+				xmax = int(min(resW, (boxes[i][3]*resW)+5))
 				cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (10,255,0), 2)
 
 				object_name = labels[int(classes[i])]
