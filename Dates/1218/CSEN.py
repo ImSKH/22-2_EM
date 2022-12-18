41034b4adad1ced1964b169fcc6c970102af6ba0
ImSKH <skihyeon1001@naver.com>
2022-12-18 19:57:17 +0900

    t

--- Dates/1218/CSEN.py
+++ Dates/1218/CSEN.py
@@ -16,7 +16,7 @@ GPIO.setup(ECHO, GPIO.IN)
 GPIO.output(TRIG, False)
 time.sleep(1)
 
-
+pre_dist = 0
 try :
 	while True:
 		GPIO.output(TRIG, True)
@@ -31,8 +31,11 @@ try :
 
 		check_time = stop - start
 		dist = check_time*34300/2
+		if dist > 1022:
+			dist = pre_dist
 		print("Distance : %.1f cm" %dist)
 		lcd.lcd_display_string("Distance : " + str(dist), 1)
+		pre_dist = dist
 		time.sleep(1)
 
 except KeyboardInterrupt:
