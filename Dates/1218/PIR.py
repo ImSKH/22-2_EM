1db9a695fb132691e538162b9683334f8aa048c1
ImSKH <skihyeon1001@naver.com>
2022-12-18 19:08:49 +0900

    t

--- Dates/1218/PIR.py
+++ Dates/1218/PIR.py
@@ -11,11 +11,17 @@ pirPin = 7
 GPIO.setup(pirPin, GPIO.IN, GPIO.PUD_UP)
 
 lcd=I2C_LCD_driver.lcd()
+lcd.backlight(1)
+try:
+    while True:
+        pir_state = GPIO.input(pirPin)
+        if pir_state == True:
+            lcd.lcd_display_string("YYYYY",1)
+            time.sleep(1)
+        else:
+            lcd.lcd_display_string("NNNNN",1)
+            time.sleep(0.1)
 
-while True:
-
-    if GPIO.input(pirPin) == GPIO.LOW:
-        lcd.lcd_display_string("YYYYY",1)
-    else:
-        lcd.lcd_display_string("NNNNN",1)
-    time.sleep(0.2)
+except KeyboardInterrupt :
+    lcd.backlight(0)
+    GPIO.cleanup()
