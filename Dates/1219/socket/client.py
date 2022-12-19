3b2aba05dea803d9ceaa136c86c50763e87698c7
ImSKH <skihyeon1001@naver.com>
2022-12-19 14:54:21 +0900

    t

---
+++ Dates/1219/socket/client.py
@@ -0,0 +1,23 @@
+##https://awakening95.tistory.com/1
+import socket
+import cv2
+
+UDP_IP = '0.0.0.1'
+UDP_PORT = 5000
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+cap = cv2.VideoCapture(cv2.CAP_V4L2+0)
+
+try:
+	while True:
+		ret, frame = cap.read()
+		d = frame.flatten()
+		s = d.tostring()
+
+		for i in range(20):
+			sock.sendto(bytes[i]+s[i*46080:(i+1)*s=46080], (UDP_IP, UDP_PORT))
+
+except KeyboardInterrupt:
+	cap.release()
+	quit()
