62c28ed7dab291da375c03e66251a75a8a8e524f
ImSKH <skihyeon1001@naver.com>
2022-12-19 22:25:09 +0900

    t

--- Dates/1219/socket_threading/client.py
+++ Dates/1219/socket_threading/client.py
@@ -1,16 +1,31 @@
 from socket import *
+import threading
+import time
 
 ip = '192.168.45.179'
 port = 8080
+
+def send(sock):
+	while True:
+		sendData = input('>>>>')
+		sock.send(sendData.encode('utf-8'))
+
+def receive(sock):
+	while True:
+		recvData = sock.recv(1024)
+		print('Client : ', recvData.decode('utf-8'))
 
 client_socket = socket(AF_INET, SOCK_STREAM)
 client_socket.connect((ip, port))
 
 print('Connected')
 
-while True:
-	recvData = client_socket.recv(1024)
-	print('Client : ', recvData.decode('utf-8'))
+sender = threading.Thread(target=send, args=(client_socket,))
+receiver = threading.Thread(target=receive, args=(client_socket,))
 
-	sendData = input('>>>>')
-	client_socket.send(sendData.encode('utf-8'))
+sender.start()
+receiver.start()
+
+while True:
+	time.sleep(1)
+	pass
--- Dates/1219/socket_threading/server.py
+++ Dates/1219/socket_threading/server.py
@@ -1,7 +1,21 @@
+##https://seolin.tistory.com/98
 from socket import *
+import threading 
+import time
+
 
 ip = '192.168.45.179'
 port = 8080
+
+def send(sock):
+	while True:
+		sendData = input('>>>>')
+		sock.send(sendData.encode('utf-8'))
+
+def receive(sock):
+	while True:
+		recvData = sock.recv(1024)
+		print('Client : ', recvData.decode('utf-8'))
 
 sever_socket = socket(AF_INET, SOCK_STREAM)
 sever_socket.bind((ip,port))
@@ -13,9 +27,12 @@ client_socket, addr = sever_socket.accept()
 
 print(str(addr), '에서 접속되었습니다.')
 
-while True:
-	sendData = input('>>>>')
-	client_socket.send(sendData.encode('utf-8'))
+sender = threading.Thread(target=send, args = (client_socket,))
+receiver = threading.Thread(target=receive, args = (client_socket,))
 
-	recvData = client_socket.recv(1024)
-	print('Client : ', recvData.decode('utf-8'))
+sender.start()
+receiver.start()
+
+while True:
+	time.sleep(1)
+	pass
