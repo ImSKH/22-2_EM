4ae7dff2733fbd18f73a8bfb0f061967a36dc815
ImSKH <skihyeon1001@naver.com>
2022-12-20 11:10:56 +0900

    t

--- Dates/1220/sock_cv_detect/client_UPD.py
+++ Dates/1220/sock_cv_detect/client_UPD.py
@@ -5,13 +5,13 @@ ip = '192.168.45.179'
 port = 8080
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-cap = cv2.VideoCapture(cv2.V4L2+0)
+cap = cv2.VideoCapture(cv2.CAP_V4L2+0)
 size = 46080
 
 while True :
 	ret, frame = cap.read()
 	d = frame.flatten()
-	s = d.tostring()
+	s = d.tobytes()
 
 	for i in range(20):
 		sock.sendto(s[i*size:(i+1)*size],(ip,port))
